9ed02c7f78c554e65664d871b3f0ca8ecaeea4b4
JvRdgz <javier.rodriguezjr97@gmail.com>
2021-02-27 17:06:19 +0100

    Sigue el mismo error. Continuar con el cub3d de Austin Swart

--- src/color.c
+++ src/color.c
@@ -16,7 +16,8 @@ void			choose_color(t_raycaster *raycaster, t_color *color)
 {
 	char	get_color;
 
-	get_color = raycaster->r_map[raycaster->map_x][raycaster->map_y];
+	// get_color = raycaster->r_map[raycaster->map_x][raycaster->map_y];
+	get_color = raycaster->world_map[raycaster->map_x][raycaster->map_y];
 	if (get_color == '1')
 		color->color = 0x00FFFF;
 	else if (get_color == '2')
